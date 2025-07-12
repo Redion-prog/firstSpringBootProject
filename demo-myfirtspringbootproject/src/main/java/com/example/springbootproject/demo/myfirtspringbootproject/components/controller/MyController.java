4eb2b634f8cb9e70e7a67d95bdfe74ef23a5b3c9
User <redion.cemeri@gmail.com>
2025-07-12 14:13:24 +0200

    r

--- demo-myfirtspringbootproject/src/main/java/com/example/springbootproject/demo/myfirtspringbootproject/components/controller/MyController.java
+++ demo-myfirtspringbootproject/src/main/java/com/example/springbootproject/demo/myfirtspringbootproject/components/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.example.springbootproject.demo.myfirtspringbootproject.components.controller
+package com.example.springbootproject.demo.myfirtspringbootproject.components.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
